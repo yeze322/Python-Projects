4cd2e4a99cb929c01efc90fb895fa58cf0e12ebb
yeze <2295905420@qq.com>
2015-03-19 00:52:06 +0800

    finish it! but still slow in qsort()

--- Markov Analyse/MarkovLibCreater.py
+++ Markov Analyse/MarkovLibCreater.py
@@ -7,7 +7,7 @@ def f_FileTokenizer(str_FileName,str_Save_FileName):
 	for line in lines_Article:
 		line = nltk.word_tokenize(line)
 		for str_Word in line:
-			fp_File.write(str_Word+'\n')
+			fp_File.write(str_Word.lower()+'\n')
 	fp_File.close()
 	return str_Save_FileName
 
@@ -100,7 +100,7 @@ class c_DictSorter:
 		return newlist
 
 
-def f_KeyFreq_Filter(dict_KeyFreq,int_ThreshouldVal=5):
+def f_KeyFreq_Filter(dict_KeyFreq,int_ThreshouldVal):
 	if int_ThreshouldVal == 0:
 		return dict_KeyFreq
 	list_KeyVal_Delete = [ ]
@@ -111,7 +111,7 @@ def f_KeyFreq_Filter(dict_KeyFreq,int_ThreshouldVal=5):
 		del(dict_KeyFreq[i])
 	return dict_KeyFreq
 
-def f_AsciiHash_to_File(list_AsciiHash,dict_KeyFreq,int_ThreshouldVal = 5):
+def f_AsciiHash_to_File(list_AsciiHash,dict_KeyFreq,int_ThreshouldVal = 0):
 	str_FileName = 'Markov_HASH.txt'
 	fp_File = open(str_FileName,'w')
 	dict_KeyFreqDic_Flited = f_KeyFreq_Filter(dict_KeyFreq,int_ThreshouldVal)
@@ -125,7 +125,7 @@ def f_AsciiHash_to_File(list_AsciiHash,dict_KeyFreq,int_ThreshouldVal = 5):
 		fp_File.write('\n')
 	fp_File.close()
 
-def f_AsciiHash_to_File_Sorted(list_AsciiHash,dict_KeyFreq,int_ThreshouldVal = 5):
+def f_AsciiHash_to_File_Sorted(list_AsciiHash,dict_KeyFreq,int_ThreshouldVal = 0):
 	str_FileName = 'Markov_HASH.txt'
 	fp_File = open(str_FileName,'w')
 	dict_KeyFreqDic_Flited = f_KeyFreq_Filter(dict_KeyFreq,int_ThreshouldVal)
@@ -140,45 +140,75 @@ def f_AsciiHash_to_File_Sorted(list_AsciiHash,dict_KeyFreq,int_ThreshouldVal = 5
 		fp_File.write('\n')
 	fp_File.close()
 
-def f_AsciiHash_to_File_Sorted_Splited(c_Markov,int_ThreshouldVal = 5): # don't want to filter
+def f_AsciiHash_to_File_Sorted_Splited(c_Markov): # don't want to filter
 	list_AsciiHash = c_Markov.list_AsciiHash
 	list_KeyFreq_Dict_asciihash = c_Markov.list_KeyFreq_Dict_asciihash
 	str_SavePath_Father = './Markov_AsciiHash/'
-	if not os.path.exsits(str_SavePath_Father):
+	if not os.path.exists(str_SavePath_Father):
 		os.mkdir(str_SavePath_Father)
 	for index_Ascii in range(256):
-		str_SavePath = str_SavePath_Father + str('index_Ascii')+'/'
+		if list_AsciiHash[index_Ascii] == { }:
+			continue
+		str_SavePath = str_SavePath_Father + str(index_Ascii)+'_'+chr(index_Ascii)+'/'
+		if not os.path.exists(str_SavePath):
+			os.mkdir(str_SavePath)
 		dict_AsciiHash_DICT = list_AsciiHash[index_Ascii]
 		dict_KeyFreq_thisAcii = list_KeyFreq_Dict_asciihash[index_Ascii]
 
-		str_Fname_Key_A = str_SavePath+'A.txt'
-		str_Fname_Key_B = str_SavePath+'B.txt'
-		str_Fname_Key_AB = str_SavePath+'A+B.txt'
-		str_Fname_Key_Freq = str_SavePath + 'A+B_Freq.txt'
-		str_Fname_NextWord = str_SavePath + 'A+B_NextWord.txt'
-		str_Fname_NextWord_Freq = str_SavePath + 'A+B_NextWord_Freq.txt'
+		str_Fname1_Key_A = str_SavePath+'A.txt'
+		str_Fname2_Key_B = str_SavePath+'B.txt'
+		str_Fname3_Key_AB = str_SavePath+'A+B.txt'
+		str_Fname4_Key_Freq = str_SavePath + 'A+B_Freq.txt'
+		str_Fname5_Key_AND_Freq = str_SavePath + 'A+B+Freq.txt'
 
-		fp_K_A = open(str_Fname_Key_A,'w')
-		fp_K_B = open(str_Fname_Key_B,'w')
-		fp_K_AB = open(str_Fname_Key_AB,'w')
-		fp_K_AB_Freq = open(str_Fname_Key_Freq,'w')
-		fp_NW = open(str_Fname_NextWord,'w')
-		fp_NW_Freq = open(str_Fname_NextWord_Freq,'w')
+		str_Fname6_NextWord = str_SavePath + 'NextWord.txt'
+		str_Fname7_NextWord_Freq = str_SavePath + 'NextWord_Freq.txt'
+		str_Fname8_NW_AND_Freq = str_SavePath + 'NextWord+Freq.txt'
+		str_Fname9_Whole = str_SavePath + 'Whole.txt'
+
+		fp1_K_A = open(str_Fname1_Key_A,'w')
+		fp2_K_B = open(str_Fname2_Key_B,'w')
+		fp3_K_AB = open(str_Fname3_Key_AB,'w')
+		fp4_K_AB_Freq = open(str_Fname4_Key_Freq,'w')
+		fp5_K_AND_Freq = open(str_Fname5_Key_AND_Freq,'w')
+
+		fp6_NW = open(str_Fname6_NextWord,'w')
+		fp7_NW_Freq = open(str_Fname7_NextWord_Freq,'w')
+		fp8_NW_AND_Freq = open(str_Fname8_NW_AND_Freq,'w')
+		fp9_Whole = open(str_Fname9_Whole,'w')
 
 		list_Key_Sorted = c_DictSorter(dict_KeyFreq_thisAcii).qsort() 
-
 		for str_Key in list_Key_Sorted:
-			list_AB = c_MarkovTool.f_cut_glue(str_Key)
-			fp_K_A.write(list_AB[0])
-			fp_K_B.write(list_AB[1])
-			fp_K_AB.write(str_Key)
+			list_AB = c_MarkovTool().f_cut_glue(str_Key)
+			fp1_K_A.write(list_AB[0]+'\n')
+			fp2_K_B.write(list_AB[1]+'\n')
+			fp3_K_AB.write(str_Key+'\n')
 			int_AB_Freq = dict_KeyFreq_thisAcii[str_Key]
-			fp_K_AB_Freq.write(str(int_AB_Freq))
-
+			fp4_K_AB_Freq.write(str(int_AB_Freq)+'\n')
+			fp5_K_AND_Freq.write(str_Key+'\t'+str(int_AB_Freq)+'\n')
+			fp9_Whole.write(str_Key+'|'+str(int_AB_Freq)+'|')
 			dict_NextWord = dict_AsciiHash_DICT[str_Key]
 			list_NextWord_sorted = c_DictSorter(dict_NextWord).insertsort()
 
 			for tail in list_NextWord_sorted:
+				fp6_NW.write(tail+'\t')
+				int_tail_Freq = dict_NextWord[tail]
+				fp7_NW_Freq.write(str(int_tail_Freq)+'\t')
+				fp8_NW_AND_Freq.write(tail+'\t'+str(int_tail_Freq)+'\t')
+				fp9_Whole.write(tail+'|'+str(int_tail_Freq)+"|")
+			fp6_NW.write('\n')
+			fp7_NW_Freq.write('\n')
+			fp8_NW_AND_Freq.write('\n')
+			fp9_Whole.write('\n')
+		fp1_K_A.close()
+		fp2_K_B.close()
+		fp3_K_AB.close()
+		fp4_K_AB_Freq.close()
+		fp5_K_AND_Freq.close()
+		fp6_NW.close()
+		fp7_NW_Freq.close()
+		fp8_NW_AND_Freq.close()
+		fp9_Whole.close()
 
 
 #dictst1 = {'a':12,'b':7,'c':55,'d':7,'e':23}
@@ -193,6 +223,8 @@ if __name__ == '__main__':
 	print 2
 	list_AsciiHash = c_Markov.list_AsciiHash
 	dict_KeyFreq = c_Markov.dict_KeyFreq
-	f_AsciiHash_to_File_Sorted(list_AsciiHash,dict_KeyFreq,8)
 	print 3
+	#f_AsciiHash_to_File(list_AsciiHash,dict_KeyFreq)
 	print 4
+	f_AsciiHash_to_File_Sorted_Splited(c_Markov)
+	print 5
